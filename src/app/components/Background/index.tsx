c5bc325cd2c08bc918f6baf0fc28e9eefc02bdce
Ueber-James <ueberjames@gmail.com>
2024-06-09 11:24:35 -0300

    troca do background

--- src/app/components/Background/index.tsx
+++ src/app/components/Background/index.tsx
@@ -6,7 +6,7 @@ export function Background() {
     return (
         <div className="fixed top-0 left-0 w-full h-full -z-10">
             <video autoPlay loop muted className="w-full h-full object-cover">
-                <source src="/Matrix.mp4" type="video/mp4" />
+                <source src="/Matrri.mp4" type="video/mp4" />
                 Seu navegador não suporta o elemento de vídeo.
             </video>
         </div>
